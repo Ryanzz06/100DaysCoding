063f121ca3f05c27f74792dca769e52004141a5c
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-10 02:26:00 -1200

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,13 @@
+class day2 {
+  public static void main(String args[]){
+        
+    boolean var1;
+    boolean var2;
+ 
+    var1 = true;
+    var2 = false;
+     
+    System.out.println("var1 = "+var1);
+    System.out.println("var2 = "+var2);  
+  }
+}
