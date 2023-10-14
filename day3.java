470ac9e24c17987912cd04e601f38ee3c3e6ce1b
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-13 23:39:46 -1200

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class day3 {
+    public static void main(String[] args){
+        String nama;
+        
+        Scanner isc = new Scanner(System.in);
+
+        System.out.print("Sebutkan nama anda : ");
+        nama = isc.nextLine();
+
+        System.out.println("Nama Saya Adalah : " + nama);
+    }
+}
