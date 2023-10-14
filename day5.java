a7114ca00112695261555d4a75da9a838e77a03d
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-13 23:40:31 -1200

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day5 {
+    public static void main(String[] args){
+        Scanner isc = new Scanner(System.in);
+        int penjumlahan,pengurangan,a,b;
+        
+        System.out.print("Masukkan Nilai A : ");
+        a = isc.nextInt();
+        System.out.print("Masukkan Nilai B : ");
+        b = isc.nextInt();
+
+        penjumlahan = a + b ;
+        pengurangan = a - b ;
+
+        System.out.println("Hasil dari penjumlahan : " + penjumlahan);
+        System.out.println("Hasil dari pengurangan : " + pengurangan);
+    }
+}
