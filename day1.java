3d93bee00cd32e15695617bb12e3d69d1b1ee361
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-21 01:19:04 -1200

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,15 @@
+public class day1 {
+    public static void main(String[] args) {
+ 
+        String nama = "MUH RYAN NURWANGSAH";
+        int usia = 20;
+        String alamat = "CAMBA UTARA";
+        String hobi = "PRAMUKA";
+
+        System.out.println("Halo, perkenalkan diri saya:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Hobi: " + hobi);
+    }
+}
