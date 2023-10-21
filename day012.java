c68e5de5eb11236363e7bfa32295043e70243450
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-21 01:09:12 -1200

    Delete day12.java

--- day012.java
+++
@@ -1,6 +0,0 @@
-public class day012 {
-    public static void main(String[] args) {
-        String[] nama = {"ryan","fadhil","yayat","edwin","haikal","efor"};
-        System.out.println(nama[0]+" "+nama[1]+" "+nama[2]+" "+nama[3]+" "+nama[4]);
-    }
-}
