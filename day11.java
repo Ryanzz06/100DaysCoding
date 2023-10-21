354f4db0ca76071b0df5fdf1919928ac6026e0d9
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-21 01:11:59 -1200

    non primitif Integer

---
+++ day11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
+        Integer umur = 20;
+        
+        System.out.print(umur);
+        
+    }
+}
+
