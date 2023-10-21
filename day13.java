b27883e0f6ce83496b968503ef94cd8a0aca3411
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-21 01:30:51 -1200

    non primitif

---
+++ day13.java
@@ -0,0 +1,10 @@
+public class day13 {
+    public static void main(String[] args) {
+        String[][] nama = {{"Ryan","D0221302"},{"Fadhil","D0221303"}}; 
+        for (int r = 0; r < nama.length ; r++){
+            System.out.println("nama : "+nama[r][0]);
+            System.out.println("NIM : "+nama[r][1]);
+        }
+    }
+    
+}
