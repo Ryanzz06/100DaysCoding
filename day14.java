b2a049cd2561d1cf307d25e114ffeae92e0a0fd7
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-24 23:45:44 -1200

    non primitif Long

---
+++ day14.java
@@ -0,0 +1,17 @@
+public class day14 {
+    public static void main(String[] args) {
+
+        Long nilai1 = Long.valueOf(900000000000000L);
+        Long nilai2 = Long.valueOf(1000000000000000L);
+
+        Long tambah = nilai1 + nilai2;
+        Long kurang = nilai1 - nilai2;
+        Long kali = nilai1 * nilai2;
+        Long bagi = nilai1/nilai2;
+
+        System.out.println("Hasil pertambahan : " + tambah);
+        System.out.println("Hasil pengurangan : "+ kurang);
+        System.out.println("Hasil perkalian : "+ kali);
+        System.out.println("Hasil pembagian : "+ bagi);
+    }
+}
