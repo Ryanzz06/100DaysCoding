165d4f0626d0f57212a5047edf62f2429cd7457f
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-25 00:04:42 -1200

    Type Casting

---
+++ day15.java
@@ -0,0 +1,27 @@
+public class day15 {
+    public static void main(String[] args) {
+        
+        double ryanDouble = 6.8;
+        int ryanInt = (int) ryanDouble; 
+        
+        System.out.println("Double : " + ryanDouble);
+        System.out.println("intejer : " + ryanInt);
+        
+        int primitif = 2003;
+        Integer nonPrimitif = primitif; 
+
+        System.out.println("primitif : " + primitif);
+        System.out.println("nonPrimitif : " + nonPrimitif);
+
+        double iscDoubel = 225.261;
+        int iscInt = (int) iscDoubel; 
+        System.out.println("double : " + iscDoubel);
+        System.out.println("intejer : " + iscInt);
+
+        int nilai = 263;
+        double nilai1 = nilai;
+
+        System.out.println("intejer : "+nilai);
+        System.out.println("double : "+nilai1);
+    }
+}
