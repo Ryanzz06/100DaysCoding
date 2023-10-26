21f4595ae8d423410967a622c229d3d84d40892b
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-26 04:46:21 -1200

    Character

---
+++ day17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day17 {
+    public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
+        char huruf;
+        System.out.print("Masukkan satu huruf bebas : ");
+        huruf = isc.next().charAt(0);
+
+        char hurufKecil = Character.toLowerCase(huruf);
+        System.out.println("Huruf Kecil : " + hurufKecil);
+
+        char hurufBesar = Character.toUpperCase(huruf);
+        System.out.println("Huruf Besar : " + hurufBesar);
+
+    }
+}
