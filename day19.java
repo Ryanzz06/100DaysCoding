5b20f215ad4801ccd70cacf2ba9986a69d7ea253
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-28 01:11:48 -1200

    konversi tipe data primitif ke string

---
+++ day19.java
@@ -0,0 +1,11 @@
+public class day19 {
+    public static void main(String[] args) {
+        int isc = 42;
+        String isc1 = String.valueOf(isc);
+        System.out.println(isc1);
+
+        double ryan = 3.14;
+        String ryan1 = String.valueOf(ryan);
+        System.out.println(ryan1);
+    }
+}
