5573552cb03722b329d6aee0fc9abeb84269cc88
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-10-31 05:29:04 -1200

    class

---
+++ day21.java
@@ -0,0 +1,23 @@
+class biodata{
+    String nama;
+    int umur;
+    double tinggiBadan;
+    byte jumlahSaudara;
+
+    public biodata(String nama,int umur,double tinggiBadan,byte jumlahSaudara) {
+        this.nama = nama ;
+        this.umur = umur;
+        this.tinggiBadan = tinggiBadan;
+        this.jumlahSaudara = jumlahSaudara;
+    }
+
+    void Lengkap(){System.out.println("Perkenalkan nama saya "+nama +" saya memiliki umur "+umur +" dan memiliki tinggi badan "+tinggiBadan+" serta saya memiliki "+jumlahSaudara +" saudara");}
+}
+
+public class day21 {
+    public static void main(String[] args) {
+        biodata ryBiodata = new biodata ("Muh ryan nurwangsah", 20, 155.7, 3); 
+        
+        ryBiodata.Lengkap();
+    }
+}
