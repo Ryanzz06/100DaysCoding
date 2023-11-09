8c034e96f19c41692b74c8d47a977c65301940dc
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-11-09 05:45:59 -1200

    Update and rename day9.java to Day009.java

--- Day009.java
+++ Day009.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class day9TipeData {
+public class Day009 {
     public static void main(String[] args) {
             Scanner isc = new Scanner(System.in);
             String nama;
