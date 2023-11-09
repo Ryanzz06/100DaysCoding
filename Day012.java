ad41914b45b7d1a36718eec560ed36a9be42e91d
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-11-09 05:47:28 -1200

    Update and rename day12.java to Day012.java

--- Day012.java
+++ Day012.java
@@ -1,4 +1,4 @@
-public class day12 {
+public class Day012 {
     public static void main(String[] args) {
         String[] nama = {"ryan","fadhil","yayat","edwin","haikal","efor"};
         System.out.println(nama[0]+" "+nama[1]+" "+nama[2]+" "+nama[3]+" "+nama[4]);
