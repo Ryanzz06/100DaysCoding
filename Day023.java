44583fcdb4f7c4d9a9aa91b87c017c8c16bd9d68
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-11-10 00:52:21 +0700

    belajar

---
+++ Day023.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class latihan {
+    public static void main(String[] args) {
+        Scanner ryan = new Scanner(System.in);
+        int x,y,p,q;
+
+        System.out.print("masukkan nilai x : ");
+        x = ryan.nextInt();
+        System.out.print("masukkan nilai y : ");
+        y = ryan.nextInt();
+
+        p = x+y;
+
+        if (p>=4) {
+            q = x*y;
+        }else{
+            q = x/y;
+        }
+        System.out.println(q);
+    }
+}
