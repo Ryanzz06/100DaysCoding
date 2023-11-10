71eea5e18ff09aeb87cbb395e9db93f2cfce75ac
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-11-10 09:34:07 +0700

    latihan uk

---
+++ Day028.py
@@ -0,0 +1,9 @@
+nilai1 = int(input("masukkan nilai 1 : "))
+nilai2 = int(input("masukkan nilai 2 : "))
+
+def total(nila1,nilai2):
+    c = nila1*nilai2
+    d = c*500000
+    return d
+
+print(total(nilai1,nilai2))
