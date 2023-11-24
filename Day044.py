17d3852583546c5869b603cf16ffca2fcd4b1f53
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-11-24 22:18:49 +0700

    luas lingkaran

---
+++ Day044.py
@@ -0,0 +1,10 @@
+def lingkaran(jari1,jari2):
+    luas = 3.14 * jari1 * jari2
+    return luas
+
+jari1 = int(input("masukkan nilai jari jari 1 "))
+jari2 = int(input("masukkan nilai jari jari 2 "))
+
+luas = lingkaran(jari1,jari2)
+
+print(luas)
