d9df8e670a2bcba20624f07debd753fd30c4c9dc
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2023-12-28 02:18:03 -1200

    Add files via upload

---
+++ Day053.py
@@ -0,0 +1,6 @@
+nama = input("masukkan nama anda : ")
+umur = int(input("berapa umur anda sekarang : "))
+tinggi_badan = float(input("masukkan tinggi badan anda : "))
+
+
+print(f"perkenalkan nama saya {nama}, saya memiliki umur {umur}, dan tinggi badan {tinggi_badan} cm.")
