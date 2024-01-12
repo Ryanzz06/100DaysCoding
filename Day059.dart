854e4dfe167df066b37e4e90eb6c3e0f9fdfb761
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-11 19:19:29 -1200

    var dart

---
+++ Day059.dart
@@ -0,0 +1,9 @@
+void main(){
+  String nama = "Muh Ryan Nurwangsah";
+  int jumlahMotor = 1;
+  double panjangMotor = 135.9;
+
+  var alamat = "Camba utara, Majene, Sulawesi Barata";
+
+  print("$nama memiliki motor $jumlahMotor dengan panjang $panjangMotor beralamat di $alamat");
+}
