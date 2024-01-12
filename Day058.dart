a76e839f03741a922551510c390bcd7d6203560e
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-11 19:14:43 -1200

    double dan bool dart

---
+++ Day058.dart
@@ -0,0 +1,11 @@
+void main(){
+  String nama = "Muh ryan nurwangsah";
+  String NIM = "D0221302";
+  int umur = 20;
+  double tinggi = 155.5;
+  bool status_nikah = false;
+
+
+  print("perkenalkan nama saya $nama dengan NIM $NIM memliki umur $umur tahun dan tinggi badan $tinggi cm dan saya status penikahan saya adalaah $status_nikah");
+
+}
