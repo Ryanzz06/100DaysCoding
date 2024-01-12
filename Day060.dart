6c334a5fb6453cf4813e3f442187f330cacde8d7
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-11 19:27:12 -1200

    var dan num dart

---
+++ Day060.dart
@@ -0,0 +1,7 @@
+void main(){
+  var namaTeman = "edwin, yayat, dan fadil";
+  num ukuranSepatu = 44;
+  num tinggi = 161.4;
+
+  print("saya memiliki teman bernama $namaTeman memiliki ukuran sepatu $ukuranSepatu cm dan masing masing memiliki tinggi badan $tinggi cm");
+}
