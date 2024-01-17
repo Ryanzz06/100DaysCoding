cc8730a738065c420ee38e2723d49e707a15a483
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-16 21:54:03 -1200

    menampilkan isi list yang kita inginkan berdasarkan index

---
+++ Day089.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  List<String> bahasaPemrograman = [];
+
+  bahasaPemrograman.add("Dart");
+  bahasaPemrograman.add("Java");
+  bahasaPemrograman.add("Python");
+
+  print("Bahasa Pemrograman pada index 1: ${bahasaPemrograman[1]}");
+
+}
