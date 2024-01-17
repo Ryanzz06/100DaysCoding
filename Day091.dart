991998058739e2b5e0d4a58c0344aa299f0c9e26
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-16 21:57:42 -1200

    menghapus isi list

---
+++ Day091.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  List<String> bahasaPemrograman = [];
+
+  bahasaPemrograman.add("Dart");
+  bahasaPemrograman.add("Java");
+  bahasaPemrograman.add("Python");
+
+  bahasaPemrograman.remove("Python");
+  print("Bahasa Pemrograman setelah menghapus: $bahasaPemrograman");
+}
