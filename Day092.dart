a87bbbeca1c89c3349e7ba6c7abec3414f66b806
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-16 21:59:10 -1200

    menampilkan jumlah isi list

---
+++ Day092.dart
@@ -0,0 +1,11 @@
+
+void main() {
+
+  List<String> bahasaPemrograman = [];
+
+  bahasaPemrograman.add("Dart");
+  bahasaPemrograman.add("Java");
+  bahasaPemrograman.add("Python");
+
+  print("Jumlah Bahasa Pemrograman: ${bahasaPemrograman.length}");
+}
