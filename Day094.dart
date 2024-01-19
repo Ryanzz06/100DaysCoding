a33977d6a7f30a232f5e881e32f99010d667623e
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-18 18:47:34 -1200

    menghapus list terakhir dart

---
+++ Day094.dart
@@ -0,0 +1,12 @@
+
+void main() {
+ 
+  List<String> namaTeman = [];
+  namaTeman.add("ryan");
+  namaTeman.add("edwin");
+  namaTeman.add("yayat");
+  namaTeman.add("fadhil");
+
+  namaTeman.removeLast();
+  print("$namaTeman");
+}
