22390fc60785e0378fbefb7e014a02eea5b270e5
Muh Ryan Nurwangsah <100202691+Ryanzz06@users.noreply.github.com>
2024-01-18 18:53:14 -1200

    menghapus panjang isi list

---
+++ Day096.dart
@@ -0,0 +1,12 @@
+
+void main() {
+ 
+  List<String> namaTeman = [];
+  namaTeman.add("ryan");
+  namaTeman.add("edwin");
+  namaTeman.add("yayat");
+  namaTeman.add("fadhil");
+
+  namaTeman.removeRange(0,2);
+  print("$namaTeman");
+}
